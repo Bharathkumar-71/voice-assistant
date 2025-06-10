58291180c0e299a6b77b360f10e18e8832b31cec
bharath kumar reddy <pbharathkumarreddy6281@gmail.com>
2025-06-10 17:05:16 +0530

    Generative.py

--- Generative.py
+++ Generative.py
@@ -7,7 +7,7 @@ def GenAi(State):
     warnings.filterwarnings('ignore', category=PendingDeprecationWarning)
     api_key = "AIzaSyDbEuwbJHCqTPgDy27ZM1azCFuLtzfCmJM"
 
-    os.environ['GOOGLE_API_KEY'] = "AIzaSyDbEuwbJHCqTPgDy27ZM1azCFuLtzfCmJM"
+    os.environ['GOOGLE_API_KEY'] = "AIuwbJHCqT7ZM1azCFuLtzfCmJM"
     genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
 
     llm = genai.GenerativeModel('gemini-pro')
